e14b82fa070ff42eac3266c2902ebbc16e3b8ca5
maxwen <max.weninger@gmail.com>
2019-11-05 15:26:07 +0100

    Partially Revert "Position "At a Glance" on type clock below status bar."
    This reverts commit a1a014827dd05bd2f1f77cb3c174f6cf772d72b4.
    
    Change-Id: I50a30247ae5c225465e7d5c8247bf5b71667cdce

--- packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -195,7 +195,6 @@ public class TypeClockController implements ClockPlugin {
         if (mDarkController != null) {
             mDarkController.setDarkAmount(darkAmount);
         }
-        mClockPosition.setDarkAmount(darkAmount);
     }
 
     @Override
