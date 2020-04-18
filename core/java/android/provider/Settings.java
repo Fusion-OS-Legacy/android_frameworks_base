d5205004305510abf755f19e753d6ac15aa6445d
Ido Ben-Hur <idoybh2@gmail.com>
2020-04-18 20:09:05 +0300

    base: Make 'R style' notification headers toggleable [1/2]
    based on 7ffad090f867432987f1341d12d0896bad27a5b2 and 52681b6f6aab8154dea56022688161f19b020069
    made to work with 26078b443222a6f0c70e7b3d4dd9daa3bc92a2ec
    
    Change-Id: I3551278f07472006c0a9fb6813831c9b42f47b73

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4801,6 +4801,14 @@ public final class Settings {
         public static final Validator LOCKSCREEN_MEDIA_METADATA_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Controls whether to show R style notification headers
+         * @hide
+         */
+        public static final String NOTIFICATION_HEADERS = "notification_headers";
+
+        private static final Validator NOTIFICATION_HEADERS_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4870,6 +4878,7 @@ public final class Settings {
             CHARGING_ANIMATION,
             QS_BLUR,
             QS_BLUR_ALPHA,
+            NOTIFICATION_HEADERS,
         };
 
         /**
@@ -5140,6 +5149,7 @@ public final class Settings {
             VALIDATORS.put(QS_BLUR, QS_BLUR_VALIDATOR);
             VALIDATORS.put(QS_BLUR_ALPHA, QS_BLUR_ALPHA_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_MEDIA_METADATA, LOCKSCREEN_MEDIA_METADATA_VALIDATOR);
+            VALIDATORS.put(NOTIFICATION_HEADERS, NOTIFICATION_HEADERS_VALIDATOR);
         }
 
         /**
